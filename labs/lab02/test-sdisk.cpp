a22c81c81c4243b2c3cb8f82d9ed5a99e7ebde93
Mark Baltes <mark.baltes@gmail.com>
2016-04-23 10:00:48 -0700

    Small modification to sdisk driver.

--- labs/lab02/test-sdisk.cpp
+++ labs/lab02/test-sdisk.cpp
@@ -8,7 +8,7 @@
 
 int main() {
     try {
-        Sdisk disk1("test1", 16, 32);
+        Sdisk disk1("test2", 16, 32);
         std::string block1, block2, block3, block4;
         for (int i=1; i<=32; i++) block1=block1+"1";
         for (int i=1; i<=32; i++) block2=block2+"2";
